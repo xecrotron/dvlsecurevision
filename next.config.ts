ab886a4bc0d29e06fff80a46b2c278635f900f8a
Lakshay <ljain@webmd.net>
2025-07-23 10:20:01 +0530

    fix: add assetpath and basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export',
+  
+  basePath: '/dvlsecurevision',
+  assetPrefix: '/dvlsecurevision',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
